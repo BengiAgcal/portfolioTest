bb4d86c9a0348d2a4ed5e4f82a7af1ffbf7e1028
BengiAgcal <bengiagcal@gmail.com>
2024-11-17 13:31:32 -0800

    over flow hidden

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import FooterContent from './FooterContent'
 const Footer = () => {
     return (
         <section className='relative h-[550px] bg-zinc-800 '
-        style={{clipPath:"polygon(0% 0, 100% 0%, 100% 100% , 0 100%)"}}>
+        style={{clipPath:"polygon(0% 0, 100% 0%, 100% 100% , 0 100%)", overflow: 'hidden'}}>
             <div className='relative h-[calc(100vh+550px)] -top-[100vh]'>
                 <div className='sticky top-[calc(100vh-550px)] h-[550px]'>
                 <FooterContent/>
