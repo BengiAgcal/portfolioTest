b36ca72706219b174d351c26053dc342856545f9
BengiAgcal <bengiagcal@gmail.com>
2024-11-19 17:04:41 -0800

    404 & loading- webp

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
     return (
         <div className="flex flex-col items-center justify-center h-screen m-auto">
             
-            <img src="/StarDiv.png" alt="star" className="rotating-image my-4" />
+            <img src="/StarDiv.webp" alt="star" className="rotating-image my-4" />
             <h2 className="text-center w-full text-6xl">Loading...</h2>
         </div>
     );
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ export default function NotFound() {
         <div className="flex flex-col items-center justify-center h-screen m-auto space-y-4">
         <div className="flex items-center justify-center">
             <h2 className=" big-text2   text-[200px]">4</h2>
-            <img src="/StarDiv.png" alt="star" className="rotating-image w-[150px] h-auto mx-4 relative bottom-0" />
+            <img src="/StarDiv.webp" alt="star" className="rotating-image w-[150px] h-auto mx-4 relative bottom-0" />
             <h2 className=" big-text2  text-[200px]">4</h2>
         </div>
         <h2 className="big-text2  italic text-center text-6xl">Page not found</h2>
