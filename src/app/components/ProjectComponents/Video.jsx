6fa377a460e815608930c9070e108dd780941950
BengiAgcal <bengiagcal@gmail.com>
2024-11-19 17:27:16 -0800

    video with fallback

--- src/app/components/ProjectComponents/Video.jsx
+++ src/app/components/ProjectComponents/Video.jsx
@@ -5,7 +5,7 @@ import { motion, useScroll, useTransform, useInView } from "framer-motion";
 const Video = ({ src, fallbackSrc }) => {
     const videoRef = useRef(null);
     const containerRef = useRef(null);
-    const [currentSrc, setCurrentSrc] = useState(src); // Manage the current source state
+    // const [currentSrc, setCurrentSrc] = useState(src); // Manage the current source state
 
     // Check if video is in view
     const isInView = useInView(videoRef, { amount: 0.2 }); // Trigger when 20% of the video is in view
@@ -27,12 +27,12 @@ const Video = ({ src, fallbackSrc }) => {
     });
     const scale = useTransform(scrollYProgress, [0, 1], [0.9, 1]);
 
-    const handleVideoError = () => {
-        // Switch to fallback source if the main source fails
-        if (fallbackSrc) {
-            setCurrentSrc(fallbackSrc);
-        }
-    };
+    // const handleVideoError = () => {
+    //     // Switch to fallback source if the main source fails
+    //     if (fallbackSrc) {
+    //         setCurrentSrc(fallbackSrc);
+    //     }
+    // };
 
     return (
         <motion.div
@@ -42,14 +42,16 @@ const Video = ({ src, fallbackSrc }) => {
         >
             <video
                 ref={videoRef}
-                src={currentSrc} // Use the state-managed source
+                //src={currentSrc} // Use the state-managed source
                 autoPlay
                 loop
                 muted
                 playsInline
-                onError={handleVideoError} // Handle errors
+                // onError={handleVideoError} // Handle errors
                 className="w-full h-auto"
             >
+                <source src={src} type="video/mp4" />
+                <source src={fallbackSrc} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </motion.div>
